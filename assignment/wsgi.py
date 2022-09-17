7814acdd80c782ba4711d89a6bfc07a454870230
KapilKumar2002 <77115844+KapilKumar2002@users.noreply.github.com>
2022-09-17 23:39:37 +0530

    Update wsgi.py

--- assignment/wsgi.py
+++ assignment/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+from dj_static import Cling
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'assignment.settings')
-
+application = Cling(get_wsgi_application())
 application = get_wsgi_application()
