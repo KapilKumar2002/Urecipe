bf9d2c83c43c35193cdb67d36c5eeca64eb9ca94
KapilKumar2002 <77115844+KapilKumar2002@users.noreply.github.com>
2022-09-27 22:38:28 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class Urecipe(models.Model):
-    recipe_image = models.ImageField(upload_to = "pics")
+    recipe_image = models.ImageField(upload_to = "pics",null=True, blank=True)
     recipe_name = models.CharField(max_length=100)
     recipe_desc = models.CharField(max_length=250)
     recipe_ingredients = models.TextField()
@@ -11,5 +11,5 @@ class Urecipe(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(User,on_delete=models.CASCADE)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.recipe_name
